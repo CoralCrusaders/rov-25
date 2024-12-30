40fe71902e6c340d6b2773b2d9477ae38f9b72b3
thexpiredpear <49598383+thexpiredpear@users.noreply.github.com>
2024-12-29 20:20:09 -0800

    i dont know whats contained in this commit

--- onboard/rpi/modes/gst_video0_localhost.c
+++
@@ -1,7 +0,0 @@
-#include "../camera_streaming/gst_camera_stream.h"
-#include <gst/gst.h>
-
-int main (int argc, char *argv[]) {
-    gst_stream_video0_localhost(argc, argv);
-    return 0;
-}
--- topside/modes/gst_video0_remote.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(parent_dir)
-import gi_stream.py
-
-def main():
-    gi_stream.play_video0()
-
-main()
